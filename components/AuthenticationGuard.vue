d9a34d13519b2ab8443f1d695339823d4f967dbd
Patrick Mäder <padi.maeder@gmail.com>
2023-11-21 23:13:03 +0100

    Create AuthenticationGuard component that prevents access to protected routes

---
+++ components/AuthenticationGuard.vue
@@ -0,0 +1,16 @@
+<template>
+  <slot v-if="isAuthenticated"></slot>
+  <div v-else class="w-full h-screen bg-secondary"></div>
+</template>
+
+<script lang="ts" setup>
+const { init, auth0ClientInitialized, isAuthenticated, login } = useAuth0();
+
+onMounted(() => {
+  if (!auth0ClientInitialized.value) init();
+})
+
+watch(isAuthenticated, () => {
+    if (!isAuthenticated.value) login();
+})
+</script>
