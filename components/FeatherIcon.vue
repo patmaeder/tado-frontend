355c1c4d56069318bf93041bee808b96eb9e3ecd
Patrick Mäder <padi.maeder@gmail.com>
2023-11-22 14:03:08 +0100

    Remove undefined attrs from icons

--- components/FeatherIcon.vue
+++ components/FeatherIcon.vue
@@ -1,15 +1,6 @@
 <template>
-  <span v-html="f.icons[props.icon]
-    .toSvg({
-      class: props.class,
-      color: props.color,
-      width: props.width,
-      height: props.height,
-      'stroke-width': props['stroke-width'] || '1.5',
-      'stroke-linecap': props['stroke-linecap'],
-      'stroke-linejoin': props['stroke-linejoin']
-  })">
-  </span>
+  <i v-if="f.icons[props.icon]" v-html="f.icons[props.icon].toSvg(attrs)">
+  </i>
 </template>
 
 <script lang="ts" setup>
@@ -17,4 +8,15 @@ import feather from 'feather-icons';
 
 const props = defineProps(['icon', 'class', 'color', 'width', 'height', 'stroke-width', 'stroke-linecap', 'stroke-linejoin']);
 const f = ref(feather);
+const attrs: { [key: string]: string } = {
+  class: props.class,
+  color: props.color,
+  width: props.width,
+  height: props.height,
+  'stroke-width': props['stroke-width'] || '1.5',
+  'stroke-linecap': props['stroke-linecap'],
+  'stroke-linejoin': props['stroke-linejoin']
+};
+
+Object.keys(attrs).forEach(key => attrs[key] === undefined && delete attrs[key])
 </script>
