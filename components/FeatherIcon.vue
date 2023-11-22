85e08506c07e353f7137cf8a5b708e48d45f5d3f
Patrick Mäder <padi.maeder@gmail.com>
2023-11-22 01:02:12 +0100

    Add feather icons

---
+++ components/FeatherIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <span v-html="f.icons[props.icon]
+    .toSvg({
+      class: props.class,
+      color: props.color,
+      width: props.width,
+      height: props.height,
+      'stroke-width': props['stroke-width'] || '1.4',
+      'stroke-linecap': props['stroke-linecap'],
+      'stroke-linejoin': props['stroke-linejoin']
+  })">
+  </span>
+</template>
+
+<script lang="ts" setup>
+import feather from 'feather-icons';
+
+const props = defineProps(['icon', 'class', 'color', 'width', 'height', 'stroke-width', 'stroke-linecap', 'stroke-linejoin']);
+const f = ref(feather);
+</script>
