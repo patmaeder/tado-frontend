83a534276404c987c70b63801e6d123b5d62f498
Patrick Mäder <padi.maeder@gmail.com>
2023-11-23 01:15:07 +0100

    Create Board and BoardDTO types

---
+++ types/Board.ts
@@ -0,0 +1,9 @@
+type Board = {
+    id: String,
+    logo: String,
+    title: String,
+    description: String,
+    appearance: 'LIGHT' | 'DARK',
+    accentColor: String,
+    tenant: String,
+}
---
+++ types/BoardDTO.ts
@@ -0,0 +1,7 @@
+type BoardDTO = {
+    logo: String,
+    title: String,
+    description: String,
+    appearance: 'LIGHT' | 'DARK',
+    accentColor: String,
+}
