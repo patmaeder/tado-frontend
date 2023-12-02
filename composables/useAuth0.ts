1efbdd3c2e831ad538a8d11e5c03702b679f628c
Patrick Mäder <padi.maeder@gmail.com>
2023-12-02 19:27:20 +0100

    Rework useAuth0 composable and move authentication guard from component to middleware

--- composables/useAuth0.ts
+++ composables/useAuth0.ts
@@ -1,53 +1,52 @@
 import {Auth0Client, type Auth0ClientOptions, createAuth0Client, User} from '@auth0/auth0-spa-js';
 
 export const useAuth0 = () => {
-    let auth0Client = ref<Auth0Client>();
-    let user = ref<User | undefined>();
-    let isAuthenticated = ref<Boolean>();
-    let accessToken = ref<String | undefined>();
+    let client = useState<Auth0Client>('client');
+    let isInitialized = useState<Boolean>('clientInitialized', () => false);
+    let user = useState<User>('user');
+    let isAuthenticated = useState<Boolean>('userIsAuthenticated');
+    let accessToken = useState<String>('accessToken');
 
-    const init = (config: Auth0ClientOptions) => {
-
+    const init = (config: Auth0ClientOptions, autoRedirect = false) => {
         return new Promise(async (resolve, reject) => {
             if (process.server) reject("Auth0Client can only be initialized clientside");
 
-            auth0Client.value = await createAuth0Client(config);
+            client.value = await createAuth0Client(config);
+
             if (location.search.includes("state=") && (location.search.includes("code=") || location.search.includes("error="))) {
-                await auth0Client.value.handleRedirectCallback();
+                await client.value.handleRedirectCallback();
                 window.history.replaceState({}, document.title, window.location.href.split('?')[0]);
             }
 
-            isAuthenticated.value = await auth0Client.value.isAuthenticated();
+            isAuthenticated.value = await client.value.isAuthenticated();
+
+            user.value = await client.value.getUser();
             if (isAuthenticated.value) {
-                user.value = await auth0Client.value.getUser();
-                accessToken.value = await auth0Client.value.getTokenSilently();
+                accessToken.value = await client.value.getTokenSilently();
             }
 
-            console.log("%c🚀 Auth0Client initiated", "padding: 2px; padding-inline: 8px; background-color: green; text-color: white; border-radius: 2px");
+            console.log("%c🚀 Auth0Client initialized", "padding: 2px; padding-inline: 8px; background-color: green; text-color: white; border-radius: 2px");
+            isInitialized.value = true;
             resolve(true);
         })
     }
 
-    const login = () => {
-        auth0Client?.value?.loginWithRedirect();
-    }
+    const login = () =>
+        new Promise((resolve, reject) => {
+            client?.value?.loginWithRedirect();
+        })
 
-    const loginWithPopup = () => {
-        auth0Client?.value?.loginWithPopup();
-    }
-
-    const logout = (redirectUrl: string) => {
-        auth0Client?.value?.logout({
+    const logout = (redirectUrl: string) =>
+        client?.value?.logout({
             logoutParams: {
                 returnTo: redirectUrl
             }
         })
-    }
 
     return {
         init,
+        isInitialized,
         login,
-        loginWithPopup,
         logout,
         user,
         isAuthenticated,
