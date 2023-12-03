73d0785b9c3e0b696dc7fffcef464dfdb4abbc38
Patrick Mäder <padi.maeder@gmail.com>
2023-12-03 02:27:18 +0100

    Add date utils

---
+++ utils/dateUtils.ts
@@ -0,0 +1,17 @@
+export const getElapsedTimeSinceCreationDate = (date: String) => {
+    const createdAt = new Date(date);
+    const now = new Date();
+
+    const timeDifference = now.getTime() - createdAt.getTime();
+    const minutesAgo = Math.floor(timeDifference / (1000 * 60));
+    const hoursAgo = Math.floor(timeDifference / (1000 * 60 * 60));
+    const daysAgo = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+    if (minutesAgo < 60) {
+        return `Vor ${ minutesAgo } ${ minutesAgo == 1 ? 'Minute' : 'Minuten' }`;
+    } else if (hoursAgo < 24) {
+        return `Vor ${ hoursAgo } ${ hoursAgo == 1 ? 'Stunde' : 'Stunden' }`;
+    } else {
+        return `Vor ${ daysAgo } ${ daysAgo == 1 ? 'Tag' : 'Tagen' }`;
+    }
+}
