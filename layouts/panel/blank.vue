b6169d6d38e4fde11c81a06b33c5c3c615bf0f25
Patrick Mäder <padi.maeder@gmail.com>
2023-12-11 22:37:52 +0100

    Add guard that shows if window size is to small to properly display panel

--- layouts/panel/blank.vue
+++ layouts/panel/blank.vue
@@ -1,5 +1,13 @@
 <template>
-  <div id="panel" class=" relative h-screen max-h-screen w-full flex flex-col bg-gray-100 overflow-hidden">
+  <div v-if="viewportToSmall" class="w-full h-screen flex flex-col items-center justify-center text-white bg-secondary">
+    <div class="max-w-3xl">
+      <p class="mb-8 text-center leading-relaxed text-lg">Die Ansicht des Panels ist nicht für mobile Endgeräte bzw. für
+        einen zu kleinen Bildausschnitt optimiert. Daher
+        ist das Panel nur in Desktopauflösung verfügbar.</p>
+      <p class="text-secondary-200 text-center">Wechseln Sie ihr Endgerät oder vergrößeren Sie ihr Browserfenster.</p>
+    </div>
+  </div>
+  <div v-else id="panel" class=" relative h-screen max-h-screen w-full flex flex-col bg-gray-100 overflow-hidden">
     <slot></slot>
   </div>
   <div v-if="notifications !== null && notifications.size > 0"
@@ -9,5 +17,12 @@
 </template>
 
 <script lang="ts" setup>
+const viewportToSmall = ref(false);
 const {notifications} = useToastNotifications();
+
+if (process.client) {
+  addEventListener("resize", () => {
+    viewportToSmall.value = window.innerWidth < 1280 || window.innerHeight < 640;
+  })
+}
 </script>
