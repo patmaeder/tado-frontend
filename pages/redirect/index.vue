60bba7f6e9e85ad0c36ef29e75106e1f910943ee
Patrick Mäder <padi.maeder@gmail.com>
2024-01-02 01:51:20 +0100

    Fix redirect after login/logout

--- pages/redirect/index.vue
+++ pages/redirect/index.vue
@@ -2,8 +2,17 @@
 
 <script lang="ts" setup>
 const route = useRoute();
-const origin = route.query["origin"];
+const origin = route.query["origin"] as string;
 delete route.query["origin"];
 
-navigateTo(origin + "?" + locationQueryToString(route.query));
+let redirect = origin;
+
+let i = 0;
+for (let key in route.query) {
+  redirect += i == 0 ? "?" : "&"
+  redirect += key + "=" + route.query[key];
+  i++;
+}
+
+navigateTo(redirect);
 </script>
